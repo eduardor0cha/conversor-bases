abafb468a401418c8374e422c76157d3fcb438ec
eduardor0cha <c.eduardo_rn@outlook.com>
2023-10-15 01:10:46 -0300

    feat/add-convertion-system

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,69 @@
+import { FormEvent, useCallback, useState } from "react";
 import { Button, Input } from "./components";
+import { checkNumber, convertBase } from "./utils/MathUtils";
+
+type ConvertionResult = {
+  number: string;
+  numberBase: number;
+  convertedNumber: string;
+  convertedNumberBase: number;
+}
 
 function App() {
+  const [error, setError] = useState<string | null>(null)
+  const [convertionResult, setConvertionResult] = useState<ConvertionResult | null>(null);
+
+  const onSubmit = useCallback((e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const number = e.currentTarget.number.value.toUpperCase().trim().replace(",", ".")
+    const originBase = e.currentTarget.originBase.value
+    const destinationBase = e.currentTarget.destinationBase.value
+
+    const checkResult = checkNumber(number, originBase);
+
+    if (checkResult != null) {
+      setError(checkResult)
+      return null;
+    }
+
+    const convertedNumber = convertBase(number, originBase, destinationBase)
+
+    setConvertionResult({
+      number: number,
+      numberBase: originBase,
+      convertedNumber: convertedNumber,
+      convertedNumberBase: destinationBase
+    })
+  }, [])
+
+  const onChange = useCallback(() => {
+    setConvertionResult(null)
+    setError(null)
+  }, [])
+
   return (
     <div className="p-home">
-      <form>
+      <form onSubmit={onSubmit} onChange={onChange} autoComplete="off">
         <div className="p-home__card">
           <h1>Conversor de bases</h1>
           <div className="p-home__number-inputs">
-            <Input label="Número" required />
-            <Input label="Base" type="number" min={2} max={36} required className="p-home__number-inputs__base" />
+            <Input label="Número" required name="number" />
+            <Input label="Base" type="number" name="originBase" min={2} max={36} required className="p-home__number-inputs__base" />
           </div>
-          <Input label="Base de destino" type="number" min={2} required max={36} className="p-home__destination-base" />
+          <Input label="Base de destino" type="number" name="destinationBase" min={2} required max={36} className="p-home__destination-base" />
           <Button type="submit">Converter</Button>
+          {error == null ? null :
+            <div className="p-home__error-container">
+              <h2>Erro</h2>
+              <p>{error}</p>
+            </div>
+          }
+          {convertionResult == null ? null :
+            <div className="p-home__result-container">
+              <h2>Resultado</h2>
+              <p>{convertionResult.number}<span>({convertionResult.numberBase})</span> = {convertionResult.convertedNumber}<span>({convertionResult.convertedNumberBase})</span></p>
+            </div>
+          }
         </div>
       </form>
     </div>
---
+++ src/utils/MathUtils.ts
@@ -0,0 +1,110 @@
+import { FROM_DECIMAL, TO_DECIMAL } from "./ConvertionDictionaries";
+
+export function checkNumber(n: string, base: number): null | string {
+  const number = n.toUpperCase().trim().replace(",", ".")
+
+  if (number.indexOf(".") !== number.lastIndexOf(".")) return "Há mais de um ponto flutuante."
+  if (number.indexOf("-") !== number.lastIndexOf("-")) return "Há mais de um sinal negativo."
+  if (number.indexOf("-") > 0) return "O sinal negativo está fora do lugar"
+
+  for (const digit of number.replace(".", "").replace("-", "")) {
+    if (TO_DECIMAL[digit] === undefined || TO_DECIMAL[digit] >= base) {
+      return "Há pelo menos um dígito inválido para a base determinada."
+    }
+  }
+
+  return null
+}
+
+function convertToDecimal(n: string, base: number): number {
+  let isNegative = false;
+  if (n[0] === "-") {
+    isNegative = true
+  }
+  const number = n.toUpperCase().trim().replace(",", ".").replace("-", "")
+  const integerPart = number.split(".")[0]
+
+
+  let decimalValue = 0
+  for (let i = 0; i < integerPart.length; i++) {
+    const digitValue = TO_DECIMAL[integerPart[(integerPart.length - 1) - i]]
+    decimalValue += digitValue * (base ** i)
+  }
+
+  if (number.indexOf(".") === -1 || number.indexOf(".") === (number.length - 1)) {
+    if (isNegative) {
+      return decimalValue * -1
+    } else {
+      return decimalValue
+    }
+  }
+
+  const floatPart = number.split(".")[1]
+
+  for (let i = 0; i < floatPart.length; i++) {
+    const digitValue = TO_DECIMAL[floatPart[i]]
+    decimalValue += digitValue / (base ** (i + 1))
+  }
+
+  if (isNegative) {
+    return decimalValue * -1
+  } else {
+    return decimalValue
+  }
+}
+
+function convertFromDecimal(n: number, base: number): string {
+  let number = n
+  let isNegative = false;
+  if (n < 0) {
+    isNegative = true
+    number *= -1
+  }
+
+  const integerPart = Math.trunc(number)
+
+  let convertedValue = ""
+
+  let aux = integerPart
+
+  while (true) {
+    convertedValue = FROM_DECIMAL[(aux % base)] + convertedValue
+
+    aux = Math.trunc(aux / base)
+    if (aux === 0) {
+      break
+    }
+  }
+
+  if (isNegative) {
+    convertedValue = "-" + convertedValue
+  }
+
+  if (Math.trunc(number) === number) return convertedValue
+
+  const floatPart = number - integerPart
+
+  let convertedFloatPart = ""
+  let aux2 = floatPart
+
+  while (convertedFloatPart.length < 16) {
+    aux2 *= base
+
+    convertedFloatPart += FROM_DECIMAL[Math.trunc(aux2)]
+    aux2 -= Math.trunc(aux2)
+
+    if (aux2 === 0) {
+      break
+    }
+  }
+
+  return [convertedValue, convertedFloatPart].join(".");
+}
+
+export function convertBase(number: string, fromBase: number, toBase: number): string {
+  if (fromBase === toBase) return number
+
+  const decimalValue = convertToDecimal(number, fromBase)
+
+  return convertFromDecimal(decimalValue, toBase)
+}
